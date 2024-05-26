ee2c1ed2dd353527568d9532e322c5b5683dad17
VJRLearning <132214566+VJRLearning@users.noreply.github.com>
2024-05-26 11:04:40 -0400

    Initial ospf.py

--- ncclient/ospf.py
+++ ncclient/ospf.py
@@ -1,1 +1,22 @@
+# set up ospf for one router
+import os
+from ncclient import manager
+from rich import print
+
+if __name__ == "__main__":
+  device = {
+    "host": "192.168.1.23",
+    "port": 830,
+    "username": "valerie",
+    "password": "cisco:,
+    "hostkey_verify": False,
+  }
+
+with manager.connec(**device) as nconf:
+  print(list(nconf.server_capabilities))
+
+with manager.connect(**device) as nconf:
+  nc_pley = nconf.get_config(source="running")
+  print(type(nc_reply))
+
 
